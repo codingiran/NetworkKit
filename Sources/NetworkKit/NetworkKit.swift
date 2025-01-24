85f4e132fe808703cb2900bd725608390fd3c4a2
CodingIran <codingiran@gmail.com>
2025-01-24 16:30:49 +0800

    fix namespace

--- Sources/NetworkKit/NetworkKit.swift
+++ Sources/NetworkKit/NetworkKit.swift
@@ -12,7 +12,5 @@ import Foundation
 #error("NetworkKit doesn't support Swift versions below 5.5.")
 #endif
 
-public enum NetworkKit: Sendable {
-    /// Current NetworkKit version Release 0.0.7. Necessary since SPM doesn't use dynamic libraries. Plus this will be more accurate.
-    public static let version = "0.0.7"
-}
+/// Current NetworkKit version Release 0.0.8. Necessary since SPM doesn't use dynamic libraries. Plus this will be more accurate.
+public let version = "0.0.8"
